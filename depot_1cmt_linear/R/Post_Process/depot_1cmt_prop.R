20183463eb2df8f04d10ab25a24ebfca30bd361a
cbdavis33 <cbdavis33@gmail.com>
2023-07-01 16:45:10 +0200

    updated depot_1cmt_linear post-processing to include shrinkage assessment

--- depot_1cmt_linear/R/Post_Process/depot_1cmt_prop.R
+++ depot_1cmt_linear/R/Post_Process/depot_1cmt_prop.R
@@ -87,59 +87,7 @@ plot(fit_loo, label_points = TRUE)
 
 draws_df <- fit$draws(format = "draws_df")
 
-## Shrinkage
-abc <- draws_df %>%
-  gather_draws(`eta_.*`[ID], regex = TRUE) %>% 
-  group_by(.draw, .variable) %>% 
-  summarize(sd_eta = sd(.value)) %>% 
-  ungroup() %>% 
-  mutate(.variable = str_remove(.variable, "eta_"))
-
-abcd <- draws_df %>%
-  gather_draws(omega_cl, omega_vc, omega_ka) %>% 
-  ungroup() %>% 
-  arrange(.draw) %>% 
-  rename(omega = ".value") %>% 
-  mutate(.variable = str_remove(.variable, "omega_"))
-
-
-(shrinkage <- abc %>% 
-    left_join(abcd, by = c(".draw", ".variable")) %>% 
-    mutate(shrinkage = 1 - sd_eta/omega) %>% 
-    select(.draw, .variable, shrinkage) %>% 
-    group_by(.variable) %>% 
-    mean_qi())
-
-## Some might calculate shrinkage this way based on point estimates, but I don't
-## think this is the right way to go about it, because it uses point estimates
-## rather than the full posterior, but here it is, anyways
-## (1 - sd(individual means)/(posterior mean of omega))
-draws_df %>%
-  gather_draws(`eta_.*`[ID], regex = TRUE) %>% 
-  summarize(estimate = mean(.value)) %>% 
-  ungroup() %>% 
-  group_by(.variable) %>% 
-  summarize(std_dev = sd(estimate)) %>% 
-  ungroup() %>% 
-  mutate(.variable = str_remove(.variable, "eta_")) %>% 
-  left_join(draws_df %>%
-              gather_draws(omega_cl, omega_vc, omega_ka) %>% 
-              summarize(estimate = mean(.value)) %>% 
-              ungroup() %>% 
-              mutate(.variable = str_remove(.variable, "omega_")),
-            by = ".variable") %>% 
-  mutate(shrinkage = 1 - std_dev/estimate)
-
-
-## Individual estimates (posterior mean)
-est_ind <- draws_df %>%
-  spread_draws(CL[ID], VC[ID], KA[ID],
-               eta_cl[ID], eta_vc[ID], eta_ka[ID]) %>% 
-  mean_qi() %>% 
-  select(ID, CL, VC,  KA, 
-         eta_cl, eta_vc, eta_ka) %>% 
-  mutate(ID = factor(ID))
-
+# Look at predictions
 post_preds_summary <- draws_df %>%
   spread_draws(pred[i], ipred[i], dv_ppc[i]) %>%
   mean_qi(pred, ipred, dv_ppc) %>%
@@ -158,9 +106,7 @@ p_dv_vs_pred <- ggplot(post_preds_summary %>%
   ylab("Observed Concentration") +
   theme(axis.text = element_text(size = 14, face = "bold"),
         axis.title = element_text(size = 18, face = "bold"),
-        axis.line = element_line(linewidth = 2)) +
-  scale_y_log10() +
-  scale_x_log10()
+        axis.line = element_line(linewidth = 2))
 
 p_dv_vs_ipred <- ggplot(post_preds_summary %>% 
                           filter(bloq == 0), aes(x = ipred, y = DV)) +
@@ -172,12 +118,17 @@ p_dv_vs_ipred <- ggplot(post_preds_summary %>%
   ylab("Observed Concentration") +
   theme(axis.text = element_text(size = 14, face = "bold"),
         axis.title = element_text(size = 18, face = "bold"),
-        axis.line = element_line(linewidth = 2)) +
-  scale_y_log10() +
-  scale_x_log10()
+        axis.line = element_line(linewidth = 2))
 
 p_dv_vs_pred + 
   p_dv_vs_ipred
+
+(p_dv_vs_pred +
+    scale_y_log10() +
+    scale_x_log10()) +
+  (p_dv_vs_ipred +
+     scale_y_log10() +
+     scale_x_log10())
 
 tmp <- ggplot(post_preds_summary) +
   ggforce::facet_wrap_paginate(~ID, labeller = label_both,
@@ -197,11 +148,11 @@ for(i in 1:ggforce::n_pages(tmp)){
           geom_line(data = post_preds_summary, 
                     mapping = aes(x = time, y = ipred, 
                                   group = ID),
-                    linetype = 1, size = 1.15) +
+                    linetype = 1, linewidth = 1.15) +
           geom_line(data = post_preds_summary, 
                     mapping = aes(x = time, y = pred, 
                                   group = ID),
-                    linetype = 2, size = 1.05) +
+                    linetype = 2, linewidth = 1.05) +
           geom_point(data = post_preds_summary %>% 
                        filter(bloq == 0), 
                      mapping = aes(x = time, y = DV, group = ID),
@@ -209,10 +160,10 @@ for(i in 1:ggforce::n_pages(tmp)){
           scale_y_continuous(name = latex2exp::TeX("$Drug\\;Conc.\\;(\\mu g/mL)$"),
                              limits = c(NA, NA),
                              trans = "identity") +
-          scale_x_continuous(name = "Time (d)",
-                             breaks = seq(0, 216, by = 24),
-                             labels = seq(0, 216/24, by = 24/24),
-                             limits = c(0, NA)) +
+          scale_x_continuous(name = "Time (h)",
+                             breaks = seq(0, max(nonmem_data$time), by = 14),
+                             labels = seq(0, max(nonmem_data$time), by = 14),
+                             limits = c(0, max(nonmem_data$time))) +
           theme_bw() +
           theme(axis.text = element_text(size = 14, face = "bold"),
                 axis.title = element_text(size = 18, face = "bold"),
@@ -223,44 +174,19 @@ for(i in 1:ggforce::n_pages(tmp)){
   
 }
 
-
-## Standardized Random Effects (posterior mean)
-eta_std <- est_ind %>% 
-  select(ID, starts_with("eta")) %>% 
-  pivot_longer(cols = starts_with("eta"), 
-               names_to = "parameter", 
-               values_to = "eta",
-               names_prefix = "eta_") %>% 
-  group_by(parameter) %>% 
-  mutate(eta_std = (eta - mean(eta))/sd(eta)) %>% 
-  ungroup() %>% 
-  mutate(parameter = toupper(parameter), 
-         parameter = factor(parameter, levels = c("CL", "VC", "KA")))
-
-## Standardized Random Effects (posterior mean) with Standard Normal overlayed
-eta_std %>% 
-  ggplot(aes(x = eta_std, group = parameter)) + 
-  geom_histogram(aes(y = after_stat(density))) + 
-  geom_density(color = "blue", size = 1.5) +
-  stat_function(fun = dnorm, args = list(mean = 0, sd = 1),
-                color = "red", size = 1) +
-  scale_x_continuous(name = "Standardized Indiv. Effect",
-                     limits = c(-2.5, 2.5)) +
-  theme_bw(18) +
-  facet_wrap(~parameter, scales = "free_x")
-
-eta_std %>% 
-  ggplot(aes(x = parameter, y = eta_std)) + 
-  geom_boxplot() +
-  scale_x_discrete(name = "Parameter") +
-  scale_y_continuous(name = "Standardized Indiv. Effect") +
-  theme_bw(18) +
-  geom_hline(yintercept = 0, linetype = "dashed")
-
+## Look at residuals and epsilon shrinkage
 residuals <- draws_df %>%
   spread_draws(res[i], wres[i], ires[i], iwres[i], ipred[i]) %>% 
   mutate(time = nonmem_data$time[nonmem_data$evid == 0][i],
          bloq = nonmem_data$bloq[nonmem_data$evid == 0][i])
+
+(shrinkage_eps <- residuals %>% 	
+    filter(bloq == 0) %>% 
+    group_by(.draw) %>% 	
+    summarize(sd_iwres = sd(iwres)) %>% 	
+    ungroup() %>% 	
+    mutate(shrinkage = 1 - sd_iwres) %>% 	
+    mean_qi(shrinkage))
 
 residuals %>% 
   filter(bloq == 0) %>%
@@ -270,7 +196,6 @@ residuals %>%
   geom_abline() +
   theme_bw() +
   facet_wrap(~.draw, labeller = label_both)
-
 
 some_residuals_qq <- residuals %>% 
   filter(bloq == 0) %>%
@@ -289,12 +214,11 @@ residuals %>%
   sample_draws(4) %>%
   ggplot(aes(x = iwres)) + 
   geom_histogram(aes(y = after_stat(density))) +
-  geom_density(color = "blue", size = 1.5) +
+  geom_density(color = "blue", linewidth = 1.5) +
   stat_function(fun = dnorm, args = list(mean = 0, sd = 1),
                 color = "red", size = 1) +
   theme_bw() +
   facet_wrap(~ .draw, labeller = label_both)
-
 
 residuals %>% 
   filter(bloq == 0) %>%
@@ -308,7 +232,7 @@ residuals %>%
              linewidth = 1.25) +
   geom_hline(aes(yintercept = qn_upper), linetype = "dashed", color = "blue",
              linewidth = 1.25) +
-  geom_smooth(se = FALSE, color = "red", linewidth = 1.5) +
+  geom_smooth(se = FALSE, color = "red", linewidth = 1.25) +
   theme_bw() +
   xlab("Time (h)") +
   facet_wrap(~ .draw, labeller = label_both)
@@ -325,7 +249,7 @@ residuals %>%
              linewidth = 1.25) +
   geom_hline(aes(yintercept = qn_upper), linetype = "dashed", color = "blue",
              linewidth = 1.25) +
-  geom_smooth(se = FALSE, color = "red", size = 1.5) +
+  geom_smooth(se = FALSE, color = "red", linewidth = 1.25) +
   theme_bw() +
   facet_wrap(~ .draw, labeller = label_both)
 
@@ -341,29 +265,39 @@ some_residuals_scatter <- residuals %>%
              linewidth = 1.25) +
   geom_hline(aes(yintercept = qn_upper), linetype = "dashed", color = "blue",
              linewidth = 1.25) +
-  geom_smooth(se = FALSE, color = "red", linewidth = 1.5) +
+  geom_smooth(se = FALSE, color = "red", linewidth = 1.25) +
   theme_bw() +
   transition_manual(.draw)
 
 animate(some_residuals_scatter, nframes = 30, width = 384, height = 384, 
         res = 96, dev = "png", type = "cairo", fps = 5)
 
+# |iwres| vs. IPRED
+residuals %>% 
+  filter(bloq == 0) %>%
+  sample_draws(9) %>% 
+  mutate(abs_iwres = abs(iwres)) %>% 
+  ggplot(aes(x = ipred, y = abs_iwres)) +
+  geom_point() + 
+  geom_smooth(method = "loess", se = FALSE, color = "red", linewidth = 1.25) +
+  theme_bw() +
+  ylab(("|IWRES|")) +
+  facet_wrap(~ .draw, labeller = label_both)
 
-# # This one is hard to see when each individual has very similar timepoints
-# residuals %>% 
-#   filter(bloq == 0) %>%
-#   ggplot(aes(x = time, y = iwres, group = i)) +
-#   stat_pointinterval(.width = c(0.95), point_size = 3) +
-#   theme_bw() +
-#   geom_hline(yintercept = 0, color = "blue", linewidth = 1.5) +
-#   scale_x_continuous(name = "Time (h)",
-#                      limits = c(NA, NA))
-
-residuals %>%
+## Some might calculate shrinkage this way based on point estimates, but I don't	
+## think this is the right way to go about it, because it uses point estimates	
+## rather than the full posterior, but here it is, anyways	
+## (1 - sd(posterior means of iwres))	
+iwres_mean <- residuals %>%
   filter(bloq == 0) %>%
   group_by(i, time) %>% 
   summarize(iwres_mean = mean(iwres)) %>% 
-  ungroup() %>% 
+  ungroup()
+
+iwres_mean %>% 
+  summarize(shrinkage = 1 - sd(iwres_mean))
+
+iwres_mean %>% 
   ggplot(aes(x = time, y = iwres_mean)) +
   theme_bw() +
   geom_point() +
@@ -372,6 +306,23 @@ residuals %>%
   scale_x_continuous(name = "Time (h)",
                      limits = c(NA, NA))
 
+iwres_mean %>% 
+  ggplot(aes(sample = iwres_mean)) + 
+  geom_qq() +
+  geom_abline() +
+  theme_bw()
+
+# # This one is hard to see when each individual has very similar timepoints
+# residuals %>%
+#   filter(bloq == 0) %>%
+#   ggplot(aes(x = time, y = iwres, group = i)) +
+#   stat_pointinterval(.width = c(0.95), point_size = 3) +
+#   theme_bw() +
+#   geom_hline(yintercept = 0, color = "blue", linewidth = 1.5) +
+#   scale_x_continuous(name = "Time (h)",
+#                      limits = c(NA, NA))
+
+## Look at eta shrinkage 
 # We can look at individual posterior densities on top of the density of the 
 # population parameter
 blah <- draws_df %>%
@@ -403,5 +354,188 @@ ggplot() +
                     values = c("red", "blue", "green")) +
   guides(color = "none") +
   ggtitle("Individual Parameter Posterior Densities") 
+
+
+## Shrinkage
+# A function to visualize the shrinkage
+plot_shrinkage <- function(.variable = c("CL", "VC", "KA"),
+                           pop_est, std_dev, ind_params, ...){
+  
+  dots <- list(...)
+  
+  .variable <- match.arg(.variable)
+  
+  x_label <- case_when(.variable == "CL" ~ "Clearance (L/h)",
+                       .variable == "VC" ~ "Central Compartment Volume (L)",
+                       .variable == "KA" ~ "Absorption Rate Constant (1/h)",
+                       .default = NA_character_ )
+  
+  p_1 <- ggplot() +
+    geom_histogram(aes(x = ind_params$ind_params,
+                       y = after_stat(density))) +
+    stat_function(fun = dlnorm, 
+                  args = list(meanlog = log(pop_est), sd = std_dev),
+                  color = "red", linewidth = 1) +
+    scale_x_continuous(name = x_label,
+                       limits = c(qlnorm(c(0.001, 0.995), 
+                                         log(pop_est), std_dev))) +
+    theme_bw()
+  
+  if(is.numeric(dots$.draw)){
+    
+    p_1 <- p_1 +
+      labs(subtitle = str_c("draw = ", dots$.draw)) +
+      theme(plot.subtitle = element_text(hjust = 0.5))
+    
+  }
+  
+  return(p_1)
+  
+}
+
+# This function stacks multiple shrinkage plots (e.g. by draw)
+plot_shrinkage_multiple <- function(draw){
+  
+  data_shrinkage_by_draw %>% 
+    filter(.draw == draw) %>% 
+    pmap(plot_shrinkage) %>% 
+    wrap_plots()
+  
+}
+
+# First look at shrinkage by draw
+(shrinkage <- draws_df %>%
+    gather_draws(`eta_.*`[ID], regex = TRUE) %>% 
+    group_by(.draw, .variable) %>% 
+    summarize(sd_eta = sd(.value)) %>% 
+    ungroup() %>% 
+    mutate(.variable = str_remove(.variable, "eta_")) %>% 
+    left_join(draws_df %>%
+                gather_draws(omega_cl, omega_vc, omega_ka) %>% 
+                ungroup() %>% 
+                arrange(.draw) %>% 
+                rename(omega = ".value") %>% 
+                mutate(.variable = str_remove(.variable, "omega_")),
+              by = c(".draw", ".variable")) %>% 
+    mutate(shrinkage = 1 - sd_eta/omega) %>% 
+    select(.draw, .variable, shrinkage) %>% 
+    group_by(.variable) %>% 
+    mean_qi())
+
+draws_for_shrinkage <- draws_df %>%
+  sample_draws(4)
+
+data_shrinkage_by_draw <- draws_for_shrinkage %>%
+  gather_draws(`TV.*`, regex = TRUE) %>%
+  # summarize(estimate = mean(.value)) %>%
+  ungroup() %>%
+  mutate(.variable = str_remove(.variable, "TV")) %>%
+  rename(pop_est = .value) %>%
+  inner_join(draws_for_shrinkage %>%
+               gather_draws(omega_cl, omega_vc, omega_ka) %>%
+               mutate(.variable = str_remove(.variable, "omega_") %>%
+                        toupper()) %>%
+               rename(std_dev = .value),
+             by = c(".draw", ".chain", ".iteration", ".variable")) %>%
+  inner_join(draws_for_shrinkage %>%
+               gather_draws(CL[ID], VC[ID], KA[ID]) %>%
+               ungroup()  %>% 
+               rename(ind_params = `.value`) %>%
+               select(-ID) %>%
+               group_by(.draw) %>%
+               nest(ind_params = ind_params) %>%
+               ungroup(),
+             by = c(".draw", ".chain", ".iteration", ".variable")) %>%
+  arrange(.draw, .variable) %>%
+  select(-.chain, -.iteration)
+
+map(data_shrinkage_by_draw %>% 
+      distinct(.draw) %>% 
+      pull(.draw), 
+    .f = plot_shrinkage_multiple) %>% 
+  wrap_plots(ncol = 1)
+
+
+## Some might calculate shrinkage this way based on point estimates, but I don't
+## think this is the right way to go about it, because it uses point estimates
+## rather than the full posterior, but here it is, anyways
+## (1 - sd(individual means)/(posterior mean of omega))
+draws_df %>%
+  gather_draws(`eta_.*`[ID], regex = TRUE) %>% 
+  summarize(estimate = mean(.value)) %>% 
+  ungroup() %>% 
+  group_by(.variable) %>% 
+  summarize(std_dev = sd(estimate)) %>% 
+  ungroup() %>% 
+  mutate(.variable = str_remove(.variable, "eta_")) %>% 
+  left_join(draws_df %>%
+              gather_draws(omega_cl, omega_vc, omega_ka) %>% 
+              summarize(estimate = mean(.value)) %>% 
+              ungroup() %>% 
+              mutate(.variable = str_remove(.variable, "omega_")),
+            by = ".variable") %>% 
+  mutate(shrinkage = 1 - std_dev/estimate)
+
+data_shrinkage_with_point_estimates <- draws_df %>%
+  gather_draws(`TV.*`, regex = TRUE) %>%
+  summarize(pop_est = mean(.value)) %>% 
+  ungroup() %>% 
+  mutate(.variable = str_remove(.variable, "TV")) %>% 
+  inner_join(draws_df %>% 
+               gather_draws(omega_cl, omega_vc, omega_ka) %>% 
+               summarize(std_dev = mean(.value)) %>% 
+               mutate(.variable = str_remove(.variable, "omega_") %>% 
+                        toupper()),
+             by = ".variable") %>% 
+  inner_join(draws_df %>%
+               gather_draws(CL[ID], VC[ID], KA[ID]) %>% 
+               summarize(ind_params = mean(.value)) %>% 
+               ungroup() %>% 
+               select(-ID) %>% 
+               nest(ind_params = ind_params), 
+             by = ".variable")
+
+pmap(data_shrinkage_with_point_estimates, 
+     plot_shrinkage) %>% 
+  wrap_plots()
+
+
+# Individual point estimates (posterior mean)
+est_ind <- draws_df %>%
+  spread_draws(CL[ID], VC[ID], KA[ID],
+               eta_cl[ID], eta_vc[ID], eta_ka[ID]) %>% 
+  mean_qi() %>% 
+  select(ID, CL, VC, KA,
+         eta_cl, eta_vc, eta_ka) %>% 
+  mutate(ID = factor(ID))
+
+## Standardized Random Effects (posterior mean)
+eta_std <- est_ind %>% 
+  select(ID, starts_with("eta")) %>% 
+  pivot_longer(cols = starts_with("eta"), 
+               names_to = "parameter", 
+               values_to = "eta",
+               names_prefix = "eta_") %>% 
+  group_by(parameter) %>% 
+  mutate(eta_std = (eta - mean(eta))/sd(eta)) %>% 
+  ungroup() %>% 
+  mutate(parameter = toupper(parameter))
+
+(est_ind %>% 
+    ggplot(aes(x = eta_cl, y = eta_vc)) + 
+    geom_point() +
+    theme_bw() + 
+    geom_smooth(method = "loess", span = 0.9) +
+    ggtitle("Point Estimates")) +
+  (draws_df %>%
+     sample_draws(10) %>%
+     spread_draws(`eta_.*`[ID], regex = TRUE) %>% 
+     ungroup()  %>% 
+     mutate(ID = factor(ID)) %>% 
+     ggplot(aes(x = eta_cl, y = eta_vc)) + 
+     geom_point() +
+     theme_bw() + 
+     geom_smooth(method = "loess", span = 0.9) +
+     ggtitle("Samples"))
 
 
