fab1636959fc839e07baf022a094f08a2359c4cb
cbdavis33 <cbdavis33@gmail.com>
2023-09-18 17:12:38 -0700

    my bad

---
+++ parallel_first_order_1cmt_linear/R/Post_Process/zero_into_depot_1cmt_prop_compare_prior_and_posterior.R
@@ -0,0 +1,195 @@
+rm(list = ls())
+cat("\014")
+
+library(trelliscopejs)
+library(cmdstanr)
+library(tidyverse)
+
+set_cmdstan_path("~/Torsten/cmdstan")
+
+nonmem_data <- read_csv(
+  "zero_into_depot_1cmt_linear/Data/zero_into_depot_1cmt_prop.csv",
+  na = ".") %>% 
+  rename_all(tolower) %>% 
+  rename(ID = "id",
+         DV = "dv") %>% 
+  mutate(DV = if_else(is.na(DV), 5555555, DV),    # This value can be anything except NA. It'll be indexed away 
+         bloq = if_else(is.na(bloq), -999, bloq)) # This value can be anything except NA. It'll be indexed away 
+
+n_subjects <- nonmem_data %>%  # number of individuals
+  distinct(ID) %>%
+  count() %>%
+  deframe()
+
+n_total <- nrow(nonmem_data)   # total number of records
+
+i_obs <- nonmem_data %>%
+  mutate(row_num = 1:n()) %>%
+  filter(evid == 0) %>%
+  select(row_num) %>%
+  deframe()
+
+n_obs <- length(i_obs)
+
+subj_start <- nonmem_data %>%
+  mutate(row_num = 1:n()) %>%
+  group_by(ID) %>%
+  slice_head(n = 1) %>%
+  ungroup() %>%
+  select(row_num) %>%
+  deframe()
+
+subj_end <- c(subj_start[-1] - 1, n_total)
+
+stan_data <- list(n_subjects = n_subjects,
+                  n_total = n_total,
+                  n_obs = n_obs,
+                  i_obs = i_obs,
+                  ID = nonmem_data$ID,
+                  amt = nonmem_data$amt,
+                  cmt = nonmem_data$cmt,
+                  evid = nonmem_data$evid,
+                  ii = nonmem_data$ii,
+                  addl = nonmem_data$addl,
+                  ss = nonmem_data$ss,
+                  time = nonmem_data$time,
+                  dv = nonmem_data$DV,
+                  subj_start = subj_start,
+                  subj_end = subj_end,
+                  lloq = nonmem_data$lloq,
+                  bloq = nonmem_data$bloq,
+                  location_tvcl = 1,
+                  location_tvvc = 8,
+                  location_tvka = 0.8,
+                  location_tvdur = 1,
+                  scale_tvcl = 1,
+                  scale_tvvc = 1,
+                  scale_tvka = 1,
+                  scale_tvdur = 1,
+                  scale_omega_cl = 0.4,
+                  scale_omega_vc = 0.4,
+                  scale_omega_ka = 0.4,
+                  scale_omega_dur = 0.4,
+                  lkj_df_omega = 2,
+                  scale_sigma_p = 0.5,
+                  prior_only = 1)
+
+model <- cmdstan_model(
+  "zero_into_depot_1cmt_linear/Stan/Fit/zero_into_depot_1cmt_prop.stan",
+  cpp_options = list(stan_threads = TRUE))
+
+priors <- model$sample(data = stan_data,
+                       seed = 235813,
+                       chains = 4,
+                       parallel_chains = 4,
+                       threads_per_chain = 24,
+                       iter_warmup = 500,
+                       iter_sampling = 1000,
+                       adapt_delta = 0.8,
+                       refresh = 100,
+                       max_treedepth = 10,
+                       init = function() list(TVCL = rlnorm(1, log(1), 0.3),
+                                              TVVC = rlnorm(1, log(30), 0.3),
+                                              TVKA = rlnorm(1, log(1), 0.3),
+                                              TVDUR = rlnorm(1, log(1), 0.3),
+                                              omega = rlnorm(4, log(0.3), 0.3),
+                                              sigma_p = rlnorm(1, log(0.2), 0.3)))
+
+
+fit <- read_rds(
+  "zero_into_depot_1cmt_linear/Stan/Fits/zero_into_depot_1cmt_prop.rds")
+
+draws_df <- fit$draws(format = "draws_df")
+
+parameters_to_summarize <- c(str_subset(fit$metadata()$stan_variables, "TV"),
+                             str_c("omega_", c("cl", "vc", "ka", "dur")),
+                             str_subset(fit$metadata()$stan_variables, "cor_"),
+                             "sigma_p")
+
+draws_all_df <- priors$draws(format = "draws_df") %>% 
+  mutate(target = "prior") %>% 
+  drop_na() %>% 
+  bind_rows(draws_df %>% 
+              mutate(target = "posterior")) %>% 
+  select(all_of(parameters_to_summarize), target) %>% 
+  pivot_longer(cols = TVCL:sigma_p, names_to = "variable", values_to = "value")
+
+(target_comparison_tv <- draws_all_df %>% 
+    filter(str_detect(variable, "TV")) %>%
+    mutate(variable = factor(variable, 
+                             levels = str_c("TV", c("CL", "VC", "KA", "DUR")))) %>% 
+    ggplot() +
+    geom_density(aes(x = value, fill = target), alpha = 0.25) +
+    theme_bw() +
+    scale_fill_manual(name = "Distribution",
+                      values = c("prior" = "blue", "posterior" = "red")) +
+    scale_x_log10() +
+    theme(legend.position = "bottom") +
+    facet_wrap(~ variable, scales = "free", nrow = 1))
+
+(target_comparison_omega <- draws_all_df %>% 
+    filter(str_detect(variable, "omega_")) %>% 
+    mutate(variable = factor(variable, 
+                             levels = str_c("omega_", c("cl", "vc", "ka", "dur"))),
+           variable = fct_recode(variable, "omega[CL]" = "omega_cl",
+                                 "omega[VC]" = "omega_vc",
+                                 "omega[KA]" = "omega_ka",
+                                 "omega[DUR]" = "omega_dur")) %>% 
+    ggplot() +
+    geom_density(aes(x = value, fill = target), alpha = 0.25) +
+    theme_bw() + 
+    scale_fill_manual(name = "Distribution",
+                      values = c("prior" = "blue", "posterior" = "red")) +
+    theme(legend.position = "bottom") +
+    facet_wrap(~ variable, scales = "free", nrow = 1, labeller = label_parsed))
+
+(target_comparison_cor <- draws_all_df %>% 
+    filter(str_detect(variable, "cor_")) %>% 
+    mutate(variable = factor(variable, 
+                             levels = c("cor_cl_vc", "cor_cl_ka", "cor_cl_dur", 
+                                        "cor_vc_ka", "cor_vc_dur", 
+                                        "cor_ka_dur")),
+           variable = fct_recode(variable, 
+                                 "rho[paste(CL, ', ', VC)]" = "cor_cl_vc",
+                                 "rho[paste(CL, ', ', KA)]" = "cor_cl_ka",
+                                 "rho[paste(CL, ', ', DUR)]" = "cor_cl_dur",
+                                 "rho[paste(VC, ', ', KA)]" = "cor_vc_ka",
+                                 "rho[paste(VC, ', ', DUR)]" = "cor_vc_dur",
+                                 "rho[paste(KA, ', ', DUR)]" = "cor_ka_dur")) %>% 
+    ggplot() +
+    geom_density(aes(x = value, fill = target), alpha = 0.25) +
+    theme_bw() + 
+    scale_fill_manual(name = "Distribution",
+                      values = c("prior" = "blue", "posterior" = "red")) +
+    theme(legend.position = "bottom") +
+    facet_wrap(~ variable, scales = "free", nrow = 1, labeller = label_parsed))
+
+
+(target_comparison_sigma <- draws_all_df %>% 
+    filter(str_detect(variable, "sigma_")) %>% 
+    mutate(variable = factor(variable, 
+                             levels = "sigma_p"),
+           variable = fct_recode(variable, "sigma[prop]" = "sigma_p")) %>% 
+    ggplot() +
+    geom_density(aes(x = value, fill = target), alpha = 0.25) +
+    theme_bw() + 
+    scale_fill_manual(name = "Distribution",
+                      values = c("prior" = "blue", "posterior" = "red")) +
+    theme(legend.position = "bottom") +
+    facet_wrap(~ variable, scales = "free", nrow = 1, labeller = label_parsed))
+
+
+layout <- c(
+  area(t = 1, l = 1, b = 1.5, r = 6),
+  area(t = 2, l = 1, b = 2.5, r = 6),
+  area(t = 3, l = 1, b = 3.5, r = 6),
+  area(t = 4, l = 3, b = 4.5, r = 4)
+)
+
+target_comparison_tv /
+  target_comparison_omega /
+  target_comparison_cor /
+  target_comparison_sigma +
+  plot_layout(guides = 'collect', 
+              design = layout) &
+  theme(legend.position = "bottom")
---
+++ parallel_first_order_1cmt_linear/Stan/Simulate/zero_into_depot_1cmt_ppa_with_cmax_tmax_auc.stan
@@ -0,0 +1,202 @@
+// First Order Absorption (oral/subcutaneous)
+// One-compartment PK Model
+// Zero-order distributive delay (like an infusion into the gut) to bring about 
+//   a delayed absorption
+// IIV on CL, VC, KA, DUR (full covariance matrix)
+// proportional plus additive error - DV = IPRED*(1 + eps_p) + eps_a
+// General ODE solution using Torsten
+// Observations are generated from a normal that is truncated below at 0
+// Since we have a normal distribution on the error, but the DV must be > 0, it
+//   generates values from a normal that is truncated below at 0
+// Output includes individual Cmax over the whole time period, Tmax between t1 
+//   and t2, AUC since 0 for every timepoint, and AUC between t1 and t2 (like a 
+//   dosing interval)
+
+functions{
+  
+  real normal_lb_rng(real mu, real sigma, real lb){
+    
+    real p_lb = normal_cdf(lb | mu, sigma);
+    real u = uniform_rng(p_lb, 1);
+    real y = mu + sigma * inv_Phi(u);
+    return y;
+
+  }
+  
+  vector depot_1cmt_ode(real t, vector y, array[] real params, 
+                        array[] real x_r, array[] int x_i){
+    
+    real cl = params[1];
+    real vc = params[2];
+    real ka = params[3];
+    
+    real t_1 = x_r[1];
+    real t_2 = x_r[2];
+    
+    real ke = cl/vc;
+    real slope = ka*y[1] - ke*y[2];
+    real x = slope > 0 && y[2]/vc > y[5] ? slope/vc : 0;
+    real z = t <= t_1 || (slope > 0 && t >= t_1 && t <= t_2) ? 1 : 0;
+    
+    vector[6] dydt;
+
+    dydt[1] = -ka*y[1];                            // depot
+    dydt[2] = slope;                               // central
+    dydt[3] = y[2];                                // AUC
+    dydt[4] = t >= t_1 && t <= t_2 ? y[2] : 0;     // AUC_t_1-t_2
+    dydt[5] = x;                                   // C_max
+    dydt[6] = z;                                   // t_max
+    
+    return dydt;
+  }
+  
+}
+data{
+  
+  int n_subjects;
+  int n_total;                  
+  
+  array[n_total] real amt;
+  array[n_total] int cmt;
+  array[n_total] int evid;
+  array[n_total] real ii;
+  array[n_total] int addl;
+  array[n_total] int ss;
+  array[n_total] real time;
+  
+  array[n_subjects] int subj_start;
+  array[n_subjects] int subj_end;
+  
+  real<lower = 0> TVCL;
+  real<lower = 0> TVVC;
+  real<lower = 0> TVKA;
+  real<lower = 0> TVDUR;
+  
+  real<lower = 0> omega_cl;
+  real<lower = 0> omega_vc;
+  real<lower = 0> omega_ka;
+  real<lower = 0> omega_dur;
+  
+  corr_matrix[4] R;  // Correlation matrix before transforming to Omega.
+                     // Can in theory change this to having inputs for
+                     // cor_cl_vc, cor_cl_ka, ... and then construct the 
+                     // correlation matrix in transformed data, but it's easy
+                     // enough to do in R
+  
+  real<lower = 0> sigma_p;
+  real<lower = 0> sigma_a;
+  real<lower = -1, upper = 1> cor_p_a;
+  
+  real<lower = 0> t_1;
+  real<lower = t_1> t_2;
+  
+}
+transformed data{
+  
+  int n_random = 4;
+  int n_cmt = 6;
+
+  vector[n_random] omega = [omega_cl, omega_vc, omega_ka, omega_dur]';
+  
+  matrix[n_random, n_random] L = cholesky_decompose(R);
+
+  vector[2] sigma = [sigma_p, sigma_a]';
+  matrix[2, 2] R_Sigma = rep_matrix(1, 2, 2);
+  R_Sigma[1, 2] = cor_p_a;
+  R_Sigma[2, 1] = cor_p_a;
+  
+  matrix[2, 2] Sigma = quad_form_diag(R_Sigma, sigma);
+  
+  array[n_cmt] real bioav = rep_array(1.0, n_cmt); // Hardcoding, but could be data or a parameter in another situation
+  array[n_cmt] real tlag = rep_array(0.0, n_cmt);  // Hardcoding, but could be data or a parameter in another situation
+  
+  array[1, 2] real x_r = {{t_1, t_2}};
+  
+}
+model{
+  
+}
+generated quantities{
+  
+  vector[n_total] ipred; // concentration with no residual error
+  vector[n_total] dv;    // concentration with residual error
+  
+  vector[n_total] auc;            // AUC 
+  vector[n_subjects] auc_t1_t2;   // AUC from t1 up to t2
+  vector[n_subjects] c_max;       // Cmax
+  vector[n_subjects] t_max;       // Tmax
+  vector[n_subjects] t_half;      // half-life
+  
+  vector[n_subjects] CL;
+  vector[n_subjects] VC;
+  vector[n_subjects] KA;
+  vector[n_subjects] DUR;
+  vector[n_subjects] KE;
+  
+  {
+  
+    vector[n_random] typical_values = to_vector({TVCL, TVVC, TVKA, TVDUR});
+    
+    matrix[n_random, n_subjects] eta;   
+    matrix[n_subjects, n_random] theta; 
+  
+    matrix[n_total, n_cmt] x_ipred;
+    
+    for(i in 1:n_subjects){
+      eta[, i] = multi_normal_cholesky_rng(rep_vector(0, n_random),
+                                           diag_pre_multiply(omega, L));
+    }
+    theta = (rep_matrix(typical_values, n_subjects) .* exp(eta))';
+
+    CL = col(theta, 1);
+    VC = col(theta, 2);
+    KA = col(theta, 3);
+    DUR = col(theta, 4);
+    KE = CL ./ VC;
+    
+    for(j in 1:n_subjects){
+      
+      array[subj_end[j] - subj_start[j] + 1] real rate = 
+              to_array_1d(to_vector(amt[subj_start[j]:subj_end[j]]) ./ DUR[j]);
+      
+      x_ipred[subj_start[j]:subj_end[j],] =
+        pmx_solve_rk45(depot_1cmt_ode,
+                       n_cmt,
+                       time[subj_start[j]:subj_end[j]],
+                       amt[subj_start[j]:subj_end[j]],
+                       rate,
+                       ii[subj_start[j]:subj_end[j]],
+                       evid[subj_start[j]:subj_end[j]],
+                       cmt[subj_start[j]:subj_end[j]],
+                       addl[subj_start[j]:subj_end[j]],
+                       ss[subj_start[j]:subj_end[j]],
+                       {CL[j], VC[j], KA[j]}, 
+                       bioav, tlag, x_r)';
+                       
+      ipred[subj_start[j]:subj_end[j]] = 
+                                x_ipred[subj_start[j]:subj_end[j], 2] ./ VC[j];
+                                
+      auc[subj_start[j]:subj_end[j]] = 
+                                x_ipred[subj_start[j]:subj_end[j], 3] ./ VC[j];
+      
+      auc_t1_t2[j] = max(x_ipred[subj_start[j]:subj_end[j], 4]) / VC[j];
+      c_max[j] = max(x_ipred[subj_start[j]:subj_end[j], 5]);
+      t_max[j] = max(x_ipred[subj_start[j]:subj_end[j], 6]) - t_1;
+      t_half[j] = log(2)/(CL[j]/VC[j]);
+    
+    }
+
+    for(i in 1:n_total){
+      if(ipred[i] == 0){
+         dv[i] = 0;
+      }else{
+        real ipred_tmp = ipred[i];
+        real sigma_tmp = sqrt(square(ipred_tmp) * Sigma[1, 1] + Sigma[2, 2] + 
+                              2*ipred_tmp*Sigma[2, 1]);
+        dv[i] = normal_lb_rng(ipred_tmp, sigma_tmp, 0.0);
+        
+      }
+    }
+  }
+}
+
