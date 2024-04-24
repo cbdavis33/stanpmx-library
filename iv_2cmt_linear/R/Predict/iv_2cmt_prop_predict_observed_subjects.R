85563af7dabb87534f1dfc32cc6c4de1a7a9cab2
cadavis <cadavis@dsi.com>
2024-04-24 18:00:35 -0400

    iv_2cmt_linear is done

--- iv_2cmt_linear/R/Predict/iv_2cmt_prop_predict_observed_subjects.R
+++ iv_2cmt_linear/R/Predict/iv_2cmt_prop_predict_observed_subjects.R
@@ -107,6 +107,12 @@ preds <- model$generate_quantities(fit,
                                    parallel_chains = 4,
                                    seed = 1234) 
 
+# preds <- model$generate_quantities(fit$draws() %>%
+#                                      thin_draws(100),
+#                                    data = stan_data,
+#                                    parallel_chains = 4,
+#                                    seed = 1234)
+
 preds_df <- preds$draws(format = "draws_df")
 
 rm(list = setdiff(ls(), c("preds_df", "new_data", "nonmem_data")))
@@ -179,8 +185,8 @@ for(i in 1:ggforce::n_pages(tmp)){
 
 ## Individual estimates (posterior mean)
 est_ind <- preds_df %>%
-  spread_draws(CL[ID], VC[ID], Q[ID], VP[ID],
-               auc_ss[ID], t_half_alpha[ID], t_half_terminal[ID]) %>% 
+  spread_draws(c(CL, VC, Q, VP,
+                 auc_ss, t_half_alpha, t_half_terminal)[ID]) %>% 
   mean_qi() %>% 
   select(ID, CL, VC, Q, VP, 
          auc_ss, t_half_alpha, t_half_terminal) %>% 
